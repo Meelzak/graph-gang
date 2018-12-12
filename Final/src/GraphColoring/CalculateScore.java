57560ef6c43cc7466a5241c154516d4839b5e387
Janneke13 <45425403+Janneke13@users.noreply.github.com>
2018-12-12 20:40:51 +0100

    added comments

--- Final/src/GraphColoring/CalculateScore.java
+++ Final/src/GraphColoring/CalculateScore.java
@@ -1,6 +1,10 @@
 package GraphColoring;
+/*
+* Short summary:
+* This is the class that calculates your score, based on how many hints you used during the game
+*/
 
-public class CalculateScore {//can be easily changed later, if we want our grading system to be different
+public class CalculateScore {
 
 //hint levels will go from less revealing to more revealing
 public static boolean hintOneUsed = false; //upper bound
@@ -12,14 +16,15 @@ public static boolean hintSixUsed = false; //new colour
 public static boolean hintSevenUsed = false; //biggest clique
 public static boolean hintEightUsed = false; //show coloured vertices
 public static boolean hintNineUsed = false; //says if you can use colours for the whole game
-public double score = 100; //this will be in percent
+public double score = 100; //score is in percent
 
+	//this can give the score (as a String) to another class, after it's calculated:
 	public String giveScore() {
 		calculate();
-		//resetHints(); //so, for the next game, the hints will be not-used again
 		return Integer.toString((int) score);
 	}
 	//calculates the score and subtracts points when hints are used
+	//it can never be lower than 0, and when you use more hints, the score gets lower and lower
 	public void calculate() {
 		if(hintOneUsed) {
 			score = (score/10 * 9.5);
@@ -49,6 +54,4 @@ public double score = 100; //this will be in percent
 			score = (score/10 * 5.0);
 		}
 	}
-	//I put the resethints method in game2 since all hintbuttons needed to set the mousetransparancy to false
-
 }
