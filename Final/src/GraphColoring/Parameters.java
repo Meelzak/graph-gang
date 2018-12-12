1a50e5bd0627479dfc4dbca1227ea9ffa68bab73
Meelzak <43891745+Meelzak@users.noreply.github.com>
2018-12-12 21:36:52 +0100

    added comments

--- Final/src/GraphColoring/Parameters.java
+++ Final/src/GraphColoring/Parameters.java
@@ -1,8 +1,9 @@
 package GraphColoring;
 
 public class Parameters {
-    public static  double maxPushOf=9;//maximales abstoßen
-    public static  double pushOfTo=300;//wie lange es gedrückt wird
+    //parameters for placing the dots
+    public static  double maxPushOf=9;//maximum push of each other
+    public static  double pushOfTo=300;//how long it lasts
 
     public static  double maxPushOfSpring=1.4;
     public static  double pushOfToSpring=100;
--- Final/src/GraphColoring/Position.java
+++ Final/src/GraphColoring/Position.java
@@ -1,6 +1,7 @@
 package GraphColoring;
 
 public class Position {
+    //position of the dots
     public double x;
     public double y;
     public Position(double x,double y){
